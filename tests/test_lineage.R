b05c642f4b81a8d990f4d32c1fca283d01672a8d
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2014-09-23 14:46:23 -0400

    1. Finished lineage (mostly) 2. Made a valie (buildable) R package 3. Started documentation and NAMESPACE fiddling

--- tests/test_lineage.R
+++ tests/test_lineage.R
@@ -3,11 +3,12 @@
 #' @date    2014.9.20
 
 #### Imports ####
-self_path <- dirname(sys.frame(1)$ofile)
-test_path <- file.path(dirname(self_path), "R")
-source(file.path(test_path, "DataCore.R"), chdir=TRUE)
-source(file.path(test_path, "SeqCore.R"), chdir=TRUE)
-source(file.path(test_path, "ClonalLineage.R"), chdir=TRUE)
+#self_path <- dirname(sys.frame(1)$ofile)
+#test_path <- file.path(dirname(self_path), "R")
+#source(file.path(test_path, "DataCore.R"), chdir=TRUE)
+#source(file.path(test_path, "SeqCore.R"), chdir=TRUE)
+#source(file.path(test_path, "ClonalLineage.R"), chdir=TRUE)
+library(alakazam)
 
 #### Run paramaters ####
 clone_file <- "/mnt/data/oconnor_mg_memory/changeo/RQ2341_HD07M/RQ2341_HD07M_db-pass_germ-pass_clone-pass.tab"
