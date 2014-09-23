551599e1d663e5fe907cfb47492d29d39c29b713
Jason Vander Heiden <jason.vanderheiden@gmail.com>
2014-09-23 16:47:45 -0400

    1. Removed load order 2. Added skeleton of package documentation

--- R/0-Alakazam.R
+++
@@ -1,6 +0,0 @@
-#' pRESTO and Change-O R toolkit
-#' 
-#' @imports ggplot2
-#' @imports igraph
-#' @imports plyr
-#' @imports stringr
---
+++ R/Alakazam.R
@@ -0,0 +1,14 @@
+#' pRESTO and Change-O R toolkit
+#' 
+#' Provides tools to analyze clones.  Includes tools to calculate diversity, 
+#' reconstruct lineages, and determinbe clonal assignment clustering threshold.
+#' 
+#' @name        alakazam
+#' @docType     package
+#' @references  pRESTO, Change-O, TIgGER, BASELINe, et al. 
+#' 
+#' @imports ggplot2
+#' @imports igraph
+#' @imports plyr
+#' @imports stringr
+NULL
