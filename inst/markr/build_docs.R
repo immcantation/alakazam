0fa8cf2227073923581fbe0ae600b84ff9d6132b
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2016-04-11 16:53:50 -0400

    Updated markr build script.

--- inst/markr/build_docs.R
+++ inst/markr/build_docs.R
@@ -1,18 +1,9 @@
-#library(staticdocs)
 library(markr)
 library(alakazam)
 
 # Directories
 pkg_path <- "/home/jason/workspace/igpipeline/alakazam"
-doc_path <- "/home/jason/workspace/igpipeline/prototype-docs/docs"
+doc_path <- "/home/jason/workspace/igpipeline/docs"
 
-# As a single step
+# Build
 build_mkdocs(pkg_path, doc_path=doc_path, yaml=F)
-
-# As individual steps
-#dir.create(doc_path, recursive=T)
-#pkg <- staticdocs::as.sd_package(pkg=pkg_path, site_path=doc_path)
-#pkg$topics <- build_md_topics(pkg)
-#pkg$vignettes <- build_md_vignettes(pkg)
-#pkg$index <- build_md_index(pkg)
-#build_yaml(pkg)
