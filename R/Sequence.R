56af68aae09c6c899512d64128e0193c65aa3e67
Susanna Marquez <susanna.marquez@yale.edu>
2016-04-13 14:30:45 -0400

    Removed the rcpp=TRUE tag from getSeqDistance, getSeqMatrix, testSeqEqual to use the rccp versions of the code. Replaced the code of collapseDuplicates with that of rcpp_collapseDuplictes. Removed the unit tests not needed.

--- R/Sequence.R
+++ R/Sequence.R
@@ -270,7 +270,6 @@ getAAMatrix <- function() {
 #'                     distance of 1. Meaning, indels of any length will increase
 #'                     the sequence distance by 1. Gap values other than -1 will 
 #'                     return a distance that does not consider indels as a special case.
-#' @param   rcpp       Use the Rcpp version of the code
 #'
 #' @return   Numerical distance between \code{seq1} and \code{seq2}.
 #' 
@@ -304,21 +303,8 @@ getAAMatrix <- function() {
 #' getSeqDistance("-TGGC", "AT--C", dist_mat=getDNAMatrix(gap=-1))
 #' 
 #' @export
-getSeqDistance <- function(seq1, seq2, dist_mat=getDNAMatrix(gap=-1), rcpp=FALSE) {
-    if (rcpp) {
-        return(rcpp_getSeqDistance(seq1, seq2, dist_mat))
-    }
-    # Convert string to character vector
-    seq1 <- unlist(strsplit(seq1, ""))
-    seq2 <- unlist(strsplit(seq2, ""))
-    valid.idx <- !(seq1 %in% c("-", ".")) | !(seq2 %in% c("-", "."))
-    seq1 <- seq1[valid.idx]
-    seq2 <- seq2[valid.idx]
-    # Calculate distance
-    d <- sapply(1:length(seq1), function(x) { dist_mat[seq1[x], seq2[x]] })
-    indels <- sum(rle(d)$values == -1)
-
-    return(sum(d[d >= 0]) + indels)
+getSeqDistance <- function(seq1, seq2, dist_mat=getDNAMatrix(gap=-1)) {
+    return(rcpp_getSeqDistance(seq1, seq2, dist_mat))
 }
 
 
@@ -335,7 +321,6 @@ getSeqDistance <- function(seq1, seq2, dist_mat=getDNAMatrix(gap=-1), rcpp=FALSE
 #'                     distance of 1. Meaning, indels of any length will increase
 #'                     the sequence distance by 1. Gap values other than -1 will 
 #'                     return a distance that does not consider indels as a special case.
-#' @param    rcpp      Use the Rcpp version of the code                    
 #'
 #' @return   A matrix of numerical distance between each entry in \code{seq}. 
 #'           If \code{seq} is a named vector, row and columns names will be added 
@@ -359,25 +344,8 @@ getSeqDistance <- function(seq1, seq2, dist_mat=getDNAMatrix(gap=-1), rcpp=FALSE
 #'              dist_mat=getDNAMatrix(gap=-1))
 #' 
 #' @export
-getSeqMatrix <- function(seq, dist_mat=getDNAMatrix(gap=-1), rcpp=F) {
-    if (rcpp) {
-        return(rcpp_getSeqMatrix(seq,dist_mat))
-    }
-    # Build distance matrix
-    n <- length(seq)
-    d_mat <- matrix(0, n, n)
-    for (i in 1:(n - 1)) {
-        for (j in (i + 1):n) {
-            d_mat[i, j] <- d_mat[j, i] <- getSeqDistance(seq[i], seq[j], dist_mat=dist_mat)
-        }
-    }
-    
-    # Set row and column names
-    if (!is.null(names(seq))) {
-        rownames(d_mat) <- colnames(d_mat) <- names(seq)
-    }
-    
-    return(d_mat)
+getSeqMatrix <- function(seq, dist_mat=getDNAMatrix(gap=-1)) {
+    return(rcpp_getSeqMatrix(seq,dist_mat))
 }
 
 
@@ -388,7 +356,6 @@ getSeqMatrix <- function(seq, dist_mat=getDNAMatrix(gap=-1), rcpp=F) {
 #' @param    seq1    character string containing a DNA sequence.
 #' @param    seq2    character string containing a DNA sequence.
 #' @param    ignore  vector of characters to ignore when testing for equality.
-#' @param    rcpp    use the Rcpp version of the code
 #' 
 #' @return   Returns \code{TRUE} if sequences are equal and \code{FALSE} if they are not.
 #'           Sequences of unequal length will always return \code{FALSE} regardless of
@@ -408,23 +375,8 @@ getSeqMatrix <- function(seq, dist_mat=getDNAMatrix(gap=-1), rcpp=F) {
 #' testSeqEqual("AT--T", "ATGGC", ignore="N")
 #' 
 #' @export
-testSeqEqual <- function(seq1, seq2, ignore=c("N", "-", ".", "?"), rcpp=F) {
-    if (rcpp) {
-        return(testSeqEqual(seq1,seq2,ignore))
-    }
-    # Test that sequences lengths are equal
-    if (stri_length(seq1) != stringi::stri_length(seq2)) {
-        return(FALSE)
-    }
-    
-    # Convert string to character vector
-    x <- unlist(strsplit(seq1, ""))
-    y <- unlist(strsplit(seq2, ""))
-
-    # Determine non-ignored positions
-    i <- !((x %in% ignore) | (y %in% ignore))
-    
-    return(all(x[i] == y[i]))
+testSeqEqual <- function(seq1, seq2, ignore=c("N", "-", ".", "?")) {
+    return(rcpp_testSeqEqual(seq1,seq2,ignore))
 }
 
 
@@ -714,272 +666,6 @@ collapseDuplicates <- function(data, id="SEQUENCE_ID", seq="SEQUENCE_IMGT",
     }
     
     # Build distance matrix
-    d_mat <- matrix(TRUE, nseq, nseq, dimnames=list(data[[id]], data[[id]]))
-    for (i in 1:(nseq - 1)) {
-        for (j in (i + 1):nseq) {
-            d_mat[i, j] <- d_mat[j, i] <- testSeqEqual(data[[seq]][i], data[[seq]][j], ignore)
-        }
-    }
-    
-    # Return input if no sequences are equal
-    if (!any(d_mat[lower.tri(d_mat, diag=F)])) {
-        if (verbose) { .printVerbose(nseq, nseq, 0) }
-        return(data)
-    }        
-    
-    # Find sequences that will cluster ambiguously
-    ambig_rows <- numeric()
-    for (i in 1:nseq) {
-        idx <- which(d_mat[i, ])
-        tmp_mat <- d_mat[idx, idx]
-        if (!all(tmp_mat)) { 
-            ambig_rows <- append(ambig_rows, i) 
-        }
-    }
-    discard_count <- length(ambig_rows)
-
-    # Return single sequence if all sequence belong to ambiguous clusters
-    if (discard_count == nrow(d_mat)) {
-        inform_len <- .informativeLength(data[[seq]])
-        if (verbose) { .printVerbose(nseq, 0, discard_count - 1) }
-        return(data[which.max(inform_len), ])
-    }
-    
-    # Exclude ambiguous sequences from clustering
-    if (discard_count > 0) {
-        d_mat <- d_mat[-ambig_rows, -ambig_rows]
-    }
-        
-    # Cluster remaining sequences into unique and duplicate sets
-    dup_taxa <-  list()
-    uniq_taxa <- character()
-    done_taxa <- character()
-    taxa_names <- rownames(d_mat)
-    for (taxa in taxa_names) {
-        # Skip taxa if previously assigned to a cluster
-        if (taxa %in% done_taxa) { next }
-            
-        # Find all zero distance taxa
-        idx <- which(d_mat[taxa, ])
-        if (length(idx) == 1) {
-            # Assign unique sequences to unique vector
-            uniq_taxa <- append(uniq_taxa, taxa_names[idx])
-        } else if (length(idx) > 1) {
-            # Assign clusters of duplicates to duplicate list            
-            dup_taxa <- c(dup_taxa, list(taxa_names[idx]))    
-        } else {
-            # Report error (should never occur)
-            stop("Error in distance matrix of collapseDuplicates")
-        }
-        # Update vector of clustered taxa
-        done_taxa <- c(done_taxa, taxa_names[idx])
-    }
-    
-    # Collapse duplicate sets and append entries to unique data.frame
-    unique_list <- list(data[data[[id]] %in% uniq_taxa, ])
-    for (taxa in dup_taxa) {
-        # Define row indices of identical sequences
-        idx <- which(data[[id]] %in% taxa)
-        tmp_df <- data[idx[1], ]
-        
-        if (length(idx) > 1) {
-            # Define set of text fields for row
-            for (f in text_fields) {
-                f_set <- na.omit(data[[f]][idx])
-                if (length(f_set) > 0) {
-                    f_set <- unlist(strsplit(f_set, sep))
-                    f_set <- sort(unique(f_set))
-                    f_val <- paste(f_set, collapse=sep)
-                } else {
-                    f_val <- NA
-                }
-                tmp_df[, f] <- f_val
-            }
-            
-            # Sum numeric fields
-            for (f in num_fields) {
-                f_set <- na.omit(data[[f]][idx])
-                if (length(f_set) > 0) { 
-                    f_val <- sum(f_set) 
-                } else { 
-                    f_val <- NA 
-                }
-                tmp_df[, f] <- f_val
-            }
-            
-            # Select sequence fields with fewest Ns
-            for (f in seq_fields) {
-                f_set <- na.omit(data[[f]][idx])
-                if (length(f_set) > 0) {
-                    f_len <- .informativeLength(f_set)
-                    f_val <- f_set[which.max(f_len)]
-                } else {
-                    f_val <- NA
-                }
-                tmp_df[, f] <- f_val
-            }
-            
-            # Assign id and sequence with least number of Ns
-            seq_set <- data[idx, c(id, seq)]
-            inform_len <- .informativeLength(seq_set[[seq]])
-            tmp_df[, c(id, seq)] <- seq_set[which.max(inform_len), c(id, seq)]
-        }
-        
-        # Add row to unique list
-        unique_list <- c(unique_list, list(tmp_df))
-    }
-    
-    # Combine all rows into unique data.frame
-    unique_df <- as.data.frame(bind_rows(unique_list))
-    
-    if (verbose) { .printVerbose(nseq, nrow(unique_df), discard_count) }
-    return(unique_df)
-}
-
-
-
-#' Remove duplicate DNA sequences and combine annotations
-#' 
-#' \code{collapseDuplicates} identifies duplicate DNA sequences, allowing for ambiguous 
-#' characters, removes the duplicate entries, and combines any associated annotations.
-#'
-#' @param    data         data.frame containing Change-O columns. The data.frame 
-#'                        must contain, at a minimum, a unique identifier column 
-#'                        and a column containg a character vector of DNA sequences.
-#' @param    id           name of the column containing sequence identifiers.
-#' @param    seq          name of the column containing DNA sequences.
-#' @param    text_fields  character vector of textual columns to collapse. The textual 
-#'                        annotations of duplicate sequences will be merged into a single 
-#'                        string with each unique value alphabetized and delimited by 
-#'                        \code{sep}.
-#' @param    num_fields   vector of numeric columns to collapse. The numeric annotations
-#'                        of duplicate sequences will be summed. 
-#' @param    seq_fields   vector of nucletoide sequence columns to collapse. The sequence 
-#'                        with the fewest numer of non-informative characters will be retained. 
-#'                        Where a non-informative character is one of \code{c("N", "-", ".", "?")}. 
-#'                        Note, this is distinct from the \code{seq} parameter which is used to 
-#'                        determine duplicates.
-#' @param    add_count    if \code{TRUE} add the column \code{COLLAPSE_COUNT} that 
-#'                        indicates the number of sequences that were collapsed to build 
-#'                        each unique entry.
-#' @param    ignore       vector of characters to ignore when testing for equality.
-#' @param    sep          character to use for delimiting collapsed annotations in the 
-#'                        \code{text_fields} columns. Defines both the input and output 
-#'                        delimiter.
-#' @param    verbose      if \code{TRUE} report the number input, discarded and output 
-#'                        sequences; if \code{FALSE} process sequences silently.
-#'                        
-#' @return   A modified \code{data} data.frame with duplicate sequences removed and 
-#'           annotation fields collapsed. 
-#' 
-#' @details
-#' \code{collapseDuplicates} identifies duplicate sequences in the \code{seq} column by
-#' testing for character identity, with consideration of IUPAC ambiguous nucleotide codes. 
-#' A cluster of sequences are considered duplicates if they are all equivalent, and no 
-#' member of the cluster is equivalent to a sequence in a different cluster. 
-#' 
-#' Textual annotations, specified by \code{text_fields}, are collapsed by taking the unique
-#' set of values within in each duplicate cluster and delimiting those values by \code{sep}.
-#' Numeric annotations, specified by \code{num_fields}, are collapsed by summing all values 
-#' in the duplicate cluster. Sequence annotations, specified by \code{seq_fields}, are 
-#' collapsed by retaining the first sequence with the fewest number of N characters.
-#' 
-#' Columns that are not specified in either \code{text_fields}, \code{num_fields}, or 
-#' \code{seq_fields} will be retained, but the value will be chosen from a random entry 
-#' amongst all sequences in a cluster of duplicates.
-#' 
-#' An ambiguous sequence is one that can be assigned to two different clusters, wherein
-#' the ambiguous sequence is equivalent to two sequences which are themselves 
-#' non-equivalent. Ambiguous sequences arise due to ambiguous characters at positions that
-#' vary across sequences, and are discarded along with their annotations. Thus, ambiguous
-#' sequences are removed as duplicates of some sequence, but do not create a potential
-#' false-positive annotation merger. Ambiguous sequences are not included in the 
-#' \code{COLLAPSE_COUNT} annotation that is added when \code{add_count=TRUE}.
-#' 
-#' @seealso  Equality is tested with \link{testSeqEqual}.  For IUPAC ambiguous 
-#'           character codes see \link{IUPAC_DNA}.
-#'
-#' @examples
-#' # Example Change-O data.frame
-#' df <- data.frame(SEQUENCE_ID=LETTERS[1:4],
-#'                  SEQUENCE_IMGT=c("CCCCTGGG", "CCCCTGGN", "NAACTGGN", "NNNCTGNN"),
-#'                  TYPE=c("IgM", "IgG", "IgG", "IgA"),
-#'                  SAMPLE=c("S1", "S1", "S2", "S2"),
-#'                  COUNT=1:4,
-#'                  stringsAsFactors=FALSE)
-#' 
-#' # Annotations are not parsed if neither text_fields nor num_fields is specified
-#' # The retained sequence annotations will be random
-#' collapseDuplicates(df, verbose=TRUE)
-#' 
-#' # Unique text_fields annotations are combined into a single string with ","
-#' # num_fields annotations are summed
-#' # Ambiguous duplicates are discarded
-#' collapseDuplicates(df, text_fields=c("TYPE", "SAMPLE"), num_fields="COUNT", 
-#'                    verbose=TRUE)
-#'
-#' # Use alternate delimiter for collapsing textual annotations
-#' collapseDuplicates(df, text_fields=c("TYPE", "SAMPLE"), num_fields="COUNT", 
-#'                    sep="/", verbose=TRUE)
-#' 
-#' # Add count of duplicates
-#' collapseDuplicates(df, text_fields=c("TYPE", "SAMPLE"), num_fields="COUNT", 
-#'                    add_count=TRUE, verbose=TRUE)
-#' 
-#' # Masking ragged ends may impact duplicate removal
-#' df$SEQUENCE_IMGT <- maskSeqEnds(df$SEQUENCE_IMGT)
-#' collapseDuplicates(df, text_fields=c("TYPE", "SAMPLE"), num_fields="COUNT", 
-#'                    add_count=TRUE, verbose=TRUE)
-#'
-#' @export
-rcpp_collapseDuplicates <- function(data, id="SEQUENCE_ID", seq="SEQUENCE_IMGT",
-                               text_fields=NULL, num_fields=NULL, seq_fields=NULL,
-                               add_count=FALSE, ignore=c("N", "-", ".", "?"), 
-                               sep=",", verbose=FALSE) {
-    # Verify column classes and exit if they are incorrect
-    if (!all(sapply(subset(data, select=text_fields), is.character))) {
-        stop("All text_fields columns must be of type 'character'")
-    }
-    if (!all(sapply(subset(data, select=num_fields), is.numeric))) {
-        stop("All num_fields columns must be of type 'numeric'")
-    }
-    if (!all(sapply(subset(data, select=seq_fields), is.character))) {
-        stop("All seq_fields columns must be of type 'character'")
-    }
-    seq_len <- stri_length(data[[seq]])
-    if (any(seq_len != seq_len[1])) {
-        warning("All sequences are not the same length")
-    }
-    
-    # Define verbose reporting function
-    .printVerbose <- function(n_total, n_unique, n_discard) {
-        cat(" FUNCTION> collapseDuplicates\n", sep="")
-        cat("    TOTAL> ", n_total, "\n", sep="")
-        cat("   UNIQUE> ", n_unique, "\n", sep="")
-        cat("COLLAPSED> ", n_total - n_unique - n_discard, "\n", sep="")
-        cat("DISCARDED> ", n_discard, "\n", sep="")
-        cat("\n")
-    }
-    
-    # Define function to count informative positions in sequences
-    .informativeLength <- function(x) {
-        stri_length(gsub("[N\\-\\.\\?]", "", x, perl=TRUE))
-    }
-    
-    # Intialize COLLAPSE_COUNT with 1 for each sequence
-    if(add_count) {
-        data[["COLLAPSE_COUNT"]] <- rep(1, nrow(data))
-        num_fields <- c(num_fields, "COLLAPSE_COUNT")
-    }
-    
-    # Return input if there are no sequences to collapse
-    nseq <- nrow(data)
-    if (nseq <= 1) { 
-        if (verbose) { .printVerbose(nseq, 1, 0) }
-        return(data)
-    }
-    
-    # Build distance matrix
     d_mat <- getDistanceMatrix(data[[seq]])
     colnames(d_mat) <- rownames(d_mat) <- data[[id]]
     
@@ -1097,7 +783,6 @@ rcpp_collapseDuplicates <- function(data, id="SEQUENCE_ID", seq="SEQUENCE_IMGT",
     if (verbose) { .printVerbose(nseq, nrow(unique_df), discard_count) }
     return(unique_df)
 }
-
 
 #### Annotation functions ####
 
