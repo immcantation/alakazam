c69ecce76c27a82bc7d1492b281519d7ee6d4be4
Susanna Marquez <susanna.marquez@yale.edu>
2016-07-12 13:29:01 -0400

    Added test for sortGenes Moved getSegment genes to testGene.R

--- tests/testthat/testGene.R
+++ tests/testthat/testGene.R
@@ -39,3 +39,72 @@ test_that("countGenes",{
                  c(0.2, 0.6, 0.2, 0.4, 0.3, 0.2, 0.1, 0.5, 0.4, 0.1, 0.4, 0.12),
                  tolerance=0.001)
 })
+
+
+test_that("getSegment", {
+    kappa_call <- c("Homsap IGKV1D-39*01 F,Homsap IGKV1-39*02 F,Homsap IGKV1-39*01",
+                    "Homsap IGKJ5*01 F")
+    
+    expect_equal(getAllele(kappa_call),c("IGKV1-39*01", "IGKJ5*01" ))
+    
+    expect_equal(getAllele(kappa_call, first=FALSE),
+                 c("IGKV1-39*01,IGKV1-39*02", "IGKJ5*01"))
+    
+    expect_equal(getAllele(kappa_call, first=FALSE, strip_d=FALSE),
+                 c("IGKV1D-39*01,IGKV1-39*02,IGKV1-39*01", "IGKJ5*01" ))
+    
+    expect_equal(getGene(kappa_call), c("IGKV1-39", "IGKJ5" ))
+    
+    expect_equal(getGene(kappa_call, first=FALSE),
+                 c("IGKV1-39", "IGKJ5"))
+    
+    expect_equal(getGene(kappa_call, first=FALSE, strip_d=FALSE),
+                 c("IGKV1D-39,IGKV1-39", "IGKJ5"))
+    
+    expect_equal(getFamily(kappa_call), c("IGKV1", "IGKJ5"))
+    
+    expect_equal(getFamily(kappa_call, first=FALSE), c("IGKV1", "IGKJ5"))
+    
+    expect_equal(getFamily(kappa_call, first=FALSE, collapse=FALSE),
+                 c("IGKV1,IGKV1,IGKV1", "IGKJ5"))
+    
+    expect_equal(getFamily(kappa_call, first=FALSE, strip_d=FALSE),
+                 c("IGKV1D,IGKV1", "IGKJ5"))
+    
+    heavy_call <- c("Homsap IGHV1-69*01 F,Homsap IGHV1-69D*01 F", 
+                    "Homsap IGHD1-1*01 F", 
+                    "Homsap IGHJ1*01 F")
+    
+    expect_equal(getAllele(heavy_call, first=FALSE),
+                 c("IGHV1-69*01", "IGHD1-1*01", "IGHJ1*01" ))
+    
+    expect_equal(getAllele(heavy_call, first=FALSE, strip_d=FALSE),
+                 c("IGHV1-69*01,IGHV1-69D*01", "IGHD1-1*01", "IGHJ1*01"))
+    
+    expect_equal(getGene(heavy_call, first=FALSE), 
+                 c( "IGHV1-69", "IGHD1-1", "IGHJ1"))
+    
+    expect_equal(getGene(heavy_call, first=FALSE, strip_d=FALSE),
+                 c("IGHV1-69,IGHV1-69D", "IGHD1-1", "IGHJ1" ))
+})
+
+test_that("sortGenes",{
+    genes <- c("IGHV1-69D*01", "IGHV1-69*01", "IGHV4-38-2*01", "IGHV1-69-2*01",
+            "IGHV2-5*01", "IGHV1-NL1*01", "IGHV1-2*01,IGHV1-2*05", 
+            "IGHV1-2","IGHV1-2*02","IGHV1-69*02")
+    # Sort genes by name
+    expected <- c("IGHV1-2", "IGHV1-2*01,IGHV1-2*05", "IGHV1-2*02",
+                  "IGHV1-69*01", "IGHV1-69D*01", "IGHV1-69*02", "IGHV1-69-2*01",
+                  "IGHV1-NL1*01", "IGHV2-5*01", "IGHV4-38-2*01")
+    sorted <- sortGenes(genes)
+    expect_equal(sorted, expected)
+    
+    # Sort genes by position in the locus
+    expected_locus <- c("IGHV1-NL1*01", "IGHV1-69-2*01", "IGHV1-69*01", 
+                        "IGHV1-69D*01", "IGHV1-69*02", "IGHV4-38-2*01",
+                        "IGHV2-5*01", "IGHV1-2", "IGHV1-2*01,IGHV1-2*05", 
+                        "IGHV1-2*02")
+    sorted_locus <- sortGenes(genes, method="pos")
+    expect_equal(sorted_locus, expected_locus)
+    
+})
