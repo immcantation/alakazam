c2058f0a5efd10cbc2b1612d87f0fd5850b48283
Susanna Marquez <susanna.marquez@yale.edu>
2016-07-12 15:39:19 -0400

    Fixed Topology tests.

--- tests/testthat/testTopology.R
+++ tests/testthat/testTopology.R
@@ -1,3 +1,4 @@
+load("../data-tests/ExampleTrees.rda")
 graph <- ExampleTrees[[5]]
 
 test_that("Test summarizeSubtrees",{
@@ -10,26 +11,23 @@ test_that("Test getPathLengths",{
     
     # Consider all nodes
     pl <- getPathLengths(graph, root="Germline")
-    expect_equal(pl$STEPS, c(1,2,0,3,2))
-    expect_equal(pl$DISTANCE, c(0,1,0,2,1))
+    expect_equal(pl$STEPS, c(1,0,2,2))
+    expect_equal(pl$DISTANCE, c(28,0,32,34))
     
     # Exclude nodes without an isotype annotation from step count
     graph_2 <- graph
-    iso <- V(graph_2)$ISOTYPE
-    iso[1] <- NA
-    graph_2 <- set_vertex_attr(graph_2, name="ISOTYPE", value=iso)
     pl_exclude <- getPathLengths(graph_2, root="Germline", field="ISOTYPE", 
                                  exclude=NA)
-    expect_equal(pl_exclude$STEPS, c(0,1,0,2,1))
-    expect_equal(pl_exclude$DISTANCE, c(0,1,0,2,1))
+    expect_equal(pl_exclude$STEPS, c(0,0,1,1))
+    expect_equal(pl_exclude$DISTANCE, c(28,0,32,34))
 })
 
 test_that("Test getMRCA",{
     mrca <- getMRCA(graph, path="steps", root="Germline")
-    expect_equal(mrca$NAME, "GN5SHBT08J0DZP")
+    expect_equal(mrca$NAME, "Inferred1")
     
     mrca_2 <- getMRCA(graph_2, path="distance", root="Germline", 
                       field="ISOTYPE", exclude=NA)
-    expect_equal( mrca_2$NAME,  c("GN5SHBT07FM4BU", "GN5SHBT07IBCBZ"))
+    expect_equal( mrca_2$NAME,  c("GN5SHBT03CABH1"))
     
 })
